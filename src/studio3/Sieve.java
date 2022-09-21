b8dec62d75725ee0481db1962cad658d8e85672d
jpfraga <jpfraga@localhost>
2022-09-21 15:51:05 -0500

    studio 3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,45 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is the n?");
+		int n = in.nextInt();
+		n = n;
+		boolean[] array = new boolean[n];
+		
+		
+		//determining if the indexes are true or false 
+		//we will declare all of the spots equal to true
+		//
+		for (int i = 0; i < array.length; i++)
+		{
+			int num = n % 2;
+			
+			if (num == 0)
+			
+			
+			
+			System.out.println(array[2]);
+		}
+		
+	
+		
+		
+		
+		int even = n%2;
+		int odd = n%3;
+		int fives = n%5;
+		
+	
+		
+
+		
+
+	}
+
+}
